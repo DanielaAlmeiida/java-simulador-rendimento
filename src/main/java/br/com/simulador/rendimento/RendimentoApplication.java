860f3dd8d33f3a10f198b6fb3de12821f1f6b309
Daniela Almeida <danielaspbrasil@gmail.com>
2023-10-26 11:55:59 -0300

    Lógica completa do código finalizado

--- src/main/java/br/com/simulador/rendimento/RendimentoApplication.java
+++ src/main/java/br/com/simulador/rendimento/RendimentoApplication.java
@@ -3,6 +3,8 @@ package br.com.simulador.rendimento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -11,54 +13,63 @@ public class RendimentoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RendimentoApplication.class, args);
 
-			Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+
+		double rendimentoPicPay = 0.00756;
+		double rendimentoAcumulado = 0;
+
+		List<Double> rendimentosMensais = new ArrayList<>();
+		List<Double> saldosMensais = new ArrayList<>();
 
 
-			var rendimentoPicPay = 0.00756;
-			double rendimentoAcumulado = 0;
+		System.out.println("***************************************************************************");
+		System.out.print("Este programa calculará quanto de rendimento você terá no cofrinho do\n"
+				+ "PicPay, com base no seu saldo inicial, salário mensal e no tempo em meses.\n" +
+				"Bem como o valor do rendimento e saldo de cada mês.\n");
+		System.out.println("***************************************************************************");
 
 
-			System.out.println("*-------------------------------------------------------------------*");
-			System.out.print("Este programa calculará quanto de rendimento você terá no cofrinho do\n"
-					+ "PicPay, com base no seu saldo inicial, salário mensal e no tempo em meses.\n" +
-					"Bem como o valor do rendimento e saldo de cada mês.\n");
-			System.out.println("*-------------------------------------------------------------------*");
+		System.out.print("Digite o saldo inicial: ");
+		double saldoInicial = scanner.nextDouble();
+
+		System.out.print("Digite o valor do investimento mensal: ");
+		double valorInvestimentoMensal = scanner.nextDouble();
+
+		System.out.print("Digite quantos meses você deseja ver o rendimento: ");
+		int meses = scanner.nextInt();
 
 
-			System.out.print("Digite o saldo inicial: ");
-			double saldoInicial = scanner.nextDouble();
+		rendimentoAcumulado = saldoInicial;
 
-			System.out.print("Digite o valor do intestimento mensal: ");
-			double valorInvestimentoMensal = scanner.nextDouble();
+		for (int mes = 0; mes < meses; mes ++) {
+			rendimentoAcumulado += valorInvestimentoMensal;
 
-			System.out.print("Digite quantos meses você deseja ver o rendimento: ");
-			int meses = scanner.nextInt();
+			double rendimentoMensal = rendimentoAcumulado * rendimentoPicPay;
+			rendimentosMensais.add(rendimentoMensal);
 
+			rendimentoAcumulado += rendimentoMensal;
+			saldosMensais.add(rendimentoAcumulado);
 
-
-			rendimentoAcumulado = saldoInicial;
-
-
-			for (int mes = 1; mes <= meses; mes ++) {
-					rendimentoAcumulado += valorInvestimentoMensal;
-					double rendimentoMensal = rendimentoAcumulado * rendimentoPicPay;
-					rendimentoAcumulado += rendimentoMensal;
-					System.out.println("-----------------------------------");
-					System.out.println("SALARIO ADICIONADO - Mês: " + mes);
-					System.out.printf("Saldo: %.2f%n ", rendimentoAcumulado);
-					System.out.printf("Este mês rendeu: %.2f%n ", rendimentoMensal);
-
-			}
+		}
 
 
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.printf("Saldo inicial: %.2f%n", saldoInicial);
+		System.out.printf("Saldo final: %.2f%n", rendimentoAcumulado);
+		System.out.printf("Quanto foi investido: %.2f%n", (saldoInicial + (valorInvestimentoMensal * meses)));
+		System.out.printf("Quanto rendeu: %.2f%n", (rendimentoAcumulado - (saldoInicial + (valorInvestimentoMensal * meses))));
 
-			System.out.println("-----------------------------------");
-			System.out.printf("Saldo inicial: %.2f%n", saldoInicial);
-			System.out.printf("Saldo final: %.2f%n", rendimentoAcumulado);
-			System.out.printf("Quanto foi investido: %.2f%n", (saldoInicial + (valorInvestimentoMensal * meses)));
-			System.out.printf("Quanto rendeu: %.2f%n", (rendimentoAcumulado - (saldoInicial + (valorInvestimentoMensal * meses))));
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println("ESTATÍSTICAS");
 
-			scanner.close();
+		for (int mes = 0; mes < meses; mes ++) {
+			System.out.println("MÊS: " + (mes + 1));
+			System.out.printf("Saldo: %.2f%n", saldosMensais.get(mes));
+			System.out.printf("Rendimento: %.2f%n", rendimentosMensais.get(mes));
+			System.out.println("---------------------");
+		}
+
+		scanner.close();
 
 	}
 }
