babffa60aac27123f89b30af13093af761b65679
Daniela Almeida <danielaspbrasil@gmail.com>
2023-10-26 09:26:49 -0300

    Melhoria no código

--- src/main/java/br/com/simulador/rendimento/RendimentoApplication.java
+++ src/main/java/br/com/simulador/rendimento/RendimentoApplication.java
@@ -13,43 +13,50 @@ public class RendimentoApplication {
 
 			Scanner scanner = new Scanner(System.in);
 
-			//Taxa de rendimento diaria 0.0003887167
-			// Por mes: 0.008163
-
-			//Taxa de rendimento diaria 0.00036
-			// Por mes: 0.00756
 
 			var rendimentoPicPay = 0.00756;
 			double rendimentoAcumulado = 0;
 
-			System.out.print("Este programa calculará quanto de rendimento você terá no cofrinho"
-					+ " do picpay, com base no seu saldo inicial, salário mensal e no tempo.\n");
+
+			System.out.println("*-------------------------------------------------------------------*");
+			System.out.print("Este programa calculará quanto de rendimento você terá no cofrinho do\n"
+					+ "PicPay, com base no seu saldo inicial, salário mensal e no tempo em meses.\n" +
+					"Bem como o valor do rendimento e saldo de cada mês.\n");
+			System.out.println("*-------------------------------------------------------------------*");
+
 
 			System.out.print("Digite o saldo inicial: ");
 			double saldoInicial = scanner.nextDouble();
 
-			System.out.print("Digite o salário mensal: ");
-			double salario = scanner.nextDouble();
+			System.out.print("Digite o valor do intestimento mensal: ");
+			double valorInvestimentoMensal = scanner.nextDouble();
 
 			System.out.print("Digite quantos meses você deseja ver o rendimento: ");
 			int meses = scanner.nextInt();
 
+
+
 			rendimentoAcumulado = saldoInicial;
 
-			//for (int ano = 0; ano <= anos; ano++) {
+
 			for (int mes = 1; mes <= meses; mes ++) {
-					rendimentoAcumulado += salario;
-					rendimentoAcumulado += (rendimentoAcumulado * rendimentoPicPay);
+					rendimentoAcumulado += valorInvestimentoMensal;
+					double rendimentoMensal = rendimentoAcumulado * rendimentoPicPay;
+					rendimentoAcumulado += rendimentoMensal;
+					System.out.println("-----------------------------------");
 					System.out.println("SALARIO ADICIONADO - Mês: " + mes);
-					System.out.println(rendimentoAcumulado);
-				}
-			// }
+					System.out.printf("Saldo: %.2f%n ", rendimentoAcumulado);
+					System.out.printf("Este mês rendeu: %.2f%n ", rendimentoMensal);
+
+			}
+
+
 
 			System.out.println("-----------------------------------");
 			System.out.printf("Saldo inicial: %.2f%n", saldoInicial);
 			System.out.printf("Saldo final: %.2f%n", rendimentoAcumulado);
-			System.out.printf("Quanto foi investido: %.2f%n", (saldoInicial + (salario * meses)));
-			System.out.printf("Quanto rendeu: %.2f%n", (rendimentoAcumulado - (saldoInicial + (salario * meses))));
+			System.out.printf("Quanto foi investido: %.2f%n", (saldoInicial + (valorInvestimentoMensal * meses)));
+			System.out.printf("Quanto rendeu: %.2f%n", (rendimentoAcumulado - (saldoInicial + (valorInvestimentoMensal * meses))));
 
 			scanner.close();
 
